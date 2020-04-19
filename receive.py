0cca570638b49bb1dc42becae5996d864b51ad72
Omar AbdElkareem <omarkimo80@gmail.com>
2020-04-20 00:52:10 +0200

    send & receive modules

---
+++ receive.py
@@ -0,0 +1,98 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import pyDes
+import imaplib
+import email
+import re
+import os
+
+
+def getPrivateKey():
+    keyPair = None
+    if os.path.exists('keyPair.pem'):
+        with open("keyPair.pem", "rb") as f:
+            keyPair = RSA.importKey(f.read())
+    if not keyPair:
+        keyPair = RSA.generate(1024)
+        with open('keyPair.pem', 'wb') as f:
+            f.write(keyPair.exportKey('PEM'))
+    return keyPair
+
+
+def decryptKey(encrypted_key, privateKey):
+    decryptor = PKCS1_OAEP.new(privateKey)
+    return decryptor.decrypt(encrypted_key)
+
+
+def decryptMessage(encrypted_message, key):
+    key = key.to_bytes(8, "big")
+    DES = pyDes.des(key, pyDes.CBC, IV=b"\0\0\0\0\0\0\0\0",
+                    pad=None, padmode=pyDes.PAD_PKCS5)
+    return DES.decrypt(encrypted_message, padmode=pyDes.PAD_PKCS5)
+
+
+def extractMessage(encrypted_key, encrypted_message):
+    privateKey = getPrivateKey()
+    sessionKey = decryptKey(encrypted_key, privateKey)
+    sessionKey = int.from_bytes(sessionKey, "big")
+    return decryptMessage(encrypted_message, sessionKey)
+
+
+def receiveEmail(email_address, password):
+    with imaplib.IMAP4_SSL("imap.gmail.com") as connection:
+        connection.login(email_address, password)
+        connection.select('inbox')
+        # get the messages that have "Security Project 01" in the subject
+        _, data = connection.search(None, '(SUBJECT "Security Project 01")')
+        #print(data)
+        message = ""
+        # get the latest message
+        _, data = connection.fetch(data[0].split()[-1], "(RFC822)")
+        #print(data)
+        for response_part in data:
+            if isinstance(response_part, tuple):
+                msg = email.message_from_string(response_part[1].decode())
+                message = msg.get_payload()[0].get_payload()
+                break
+        if not message:
+            print("Error, there is no received message.")
+            exit(-1)
+        Key_Message = list(map(int, re.findall('[0-9]+', str(message))))
+        lk = Key_Message[0]
+        lm = Key_Message[2]
+        intk = Key_Message[1]
+        intm = Key_Message[3]
+        return (intk.to_bytes(lk, "big"), intm.to_bytes(lm, "big"))
+
+
+def main():
+    print("Enter 'c' without quotes to enter your email and password through console, ")
+    option = input(
+        "or Enter 'f' without quotes to enter your email and password using a file: ")
+    email = ""
+    password = ""
+    if option == 'f':
+        print("You choose to enter your email and password using a file.")
+        print("Note that the structure of the file must be as follows with each at a line:")
+        print("Your_Email [example: omar.mohamed97@eng-st.cu.edu.eg]")
+        print("Your_password")
+        file_path = input("Enter the path of the file: ")
+        try:
+            with open(file_path, 'r') as f:
+                email = f.readline().rstrip()
+                password = f.readline().rstrip()
+        except:
+            print(
+                "An error happen while reading from file,\nplease check the path and the structure of the file.")
+            exit(-1)
+    else:
+        email = input("Enter your email address: ")
+        password = input("Enter your password: ")
+    received_email = receiveEmail(email, password)
+    received_message = extractMessage(
+        received_email[0], received_email[1]).decode("utf-8")
+    print("Received message:\n" + received_message)
+
+
+if __name__ == "__main__":
+    main()
---
+++ send.py
@@ -0,0 +1,106 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import pyDes
+import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import os
+import random
+
+
+def getPublicKey():
+    keyPair = None
+    if os.path.exists('keyPair.pem'):
+        with open("keyPair.pem", "rb") as f:
+            keyPair = RSA.importKey(f.read())
+    if not keyPair:
+        keyPair = RSA.generate(1024)
+        with open('keyPair.pem', 'wb') as f:
+            f.write(keyPair.exportKey('PEM'))
+    return keyPair.publickey()
+
+
+def generateKey():
+    key = 0
+    bits = (7, 8)   # 7 * 8 = 56
+    for _ in range(bits[1]):
+        key *= 2
+        for _ in range(bits[0]):
+            key *= 2
+            key += random.randint(0, 1)
+    return key
+
+
+def encryptKey(key, publicKey):
+    encryptor = PKCS1_OAEP.new(publicKey)
+    return encryptor.encrypt(key.to_bytes(8, "big"))
+
+
+def encryptMessage(message, key):
+    key = key.to_bytes(8, "big")
+    DES = pyDes.des(key, pyDes.CBC, IV=b"\0\0\0\0\0\0\0\0",
+                    pad=None, padmode=pyDes.PAD_PKCS5)
+    return DES.encrypt(message.encode())
+
+
+def sendEmail(encrypted_key, encrypted_message, sender_email, sender_password, receiver_email):
+    lk = len(encrypted_key)
+    lm = len(encrypted_message)
+    intk = int.from_bytes(encrypted_key, "big", signed=False)
+    intm = int.from_bytes(encrypted_message, "big", signed=False)
+    message = str(lk) + '-' + str(intk) + '-' + str(lm) + '-' + str(intm)
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = 'Security Project 01 - Omar AbdElkareem'
+    msg.attach(MIMEText(message, 'plain'))
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", context=context) as connection:
+        connection.login(sender_email, sender_password)
+        connection.sendmail(sender_email, receiver_email, msg.as_string())
+        print("E-mail Sent, congratulations ^_^")
+
+
+def main():
+    print("Enter 'c' without quotes to enter emails and your password through console, ")
+    option = input(
+        "or Enter 'f' without quotes to enter emails and your password using a file: ")
+    sender_email = ""
+    receiver_email = ""
+    sender_password = ""
+    message = ""
+    if option == 'f':
+        print("You chose to enter emails and the password using a file.")
+        print("Note that the structure of the file must be as follows with each at a line:")
+        print("Your_Email [example: omarkimo80@gmail.com]")
+        print("Your_password")
+        print("Receiver_Email [example: omar.mohamed97@eng-st.cu.edu.eg]")
+        print("Your_message [example: 'Hello world', it can be multi line]")
+        file_path = input("Enter the path of the file: ")
+        try:
+            with open(file_path, 'r') as f:
+                sender_email = f.readline().rstrip()
+                sender_password = f.readline().rstrip()
+                receiver_email = f.readline().rstrip()
+                message = ''.join(f.readlines()).rstrip()
+        except:
+            print(
+                "An error happen while reading from file,\nplease check the path and the structure of the file.")
+            exit(-1)
+    else:
+        sender_email = input("Enter your email address: ")
+        sender_password = input("Enter your password: ")
+        receiver_email = input("Enter receiver email address: ")
+        message = input("Enter the message: ")
+    publicKey = getPublicKey()
+    sessionKey = generateKey()
+    #sessionKey = int.from_bytes(os.urandom(7), "big")
+    encrypted_key = encryptKey(sessionKey, publicKey)
+    encrypted_message = encryptMessage(message, sessionKey)
+    sendEmail(encrypted_key, encrypted_message,
+              sender_email, sender_password, receiver_email)
+
+
+if __name__ == "__main__":
+    main()
